6e24354c3ee02d525eb4f1cb9383171ff03a14c8
Joe Bloggs <joebloggsnz@gmail.com>
2020-04-09 22:15:32 +1200

    Edited Home and About pages

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -2,6 +2,17 @@
 	<title>About</title>
 </svelte:head>
 
-<h1>About this site</h1>
+<header>
+	<h1>About this site</h1>
+</header>
+<main>
+	<img class="float-left" src="http://4.bp.blogspot.com/-68utuJdXjvs/T5WVLv4q9BI/AAAAAAAADZI/wLAQN7LpfZc/s320/Nico-van-gageldonk.jpg" alt="Nico van Gageldonk" />
+	<p>This site is where I put stuff that I think is interesting. For instance this guy with his bicycle. I found the image using a wikimedia image search. Judging by the file name his name is Nico van Gageldonk.</p>
+</main>
 
-<p>This is the 'about' page. There's not much here.</p>
+<style>
+	.float-left {
+	 float: left;
+	 margin: 0 1rem 1rem 0;
+	}	
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -15,35 +15,36 @@
 </script>
 
 <style>
-	h1, figure, p {
+	h1, h2, h3, figure, p {
 		text-align: center;
-		margin: 0 auto;
+		margin: 1em auto;
 	}
 
 	h1 {
-		font-size: 2.8em;
+		font-size: 2.25rem;
 		text-transform: uppercase;
 		font-weight: 700;
 		margin: 0 0 0.5em 0;
 	}
 
 	figure {
-		margin: 0 0 1em 0;
+		margin: 0 0 1rem 0;
 	}
 
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
+	img {		
+		max-width: 100%;
+		margin: 0 0 1rem 0;
+		box-shadow: 0 2px 4px rgba(0, 0, 0, 0.26);
+		border-radius: 5px;
 	}
 
 	p {
-		margin: 1em auto;
+		margin: 1rem auto 2rem 0;
 	}
 
 	@media (min-width: 480px) {
 		h1 {
-			font-size: 4em;
+			font-size: 3em;
 		}
 	}
 </style>
@@ -53,11 +54,13 @@
 	<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
 </svelte:head>
 
-<h1>Great success!</h1>
+<h1>Peter T's Blog</h1>
+<h2>Also known as 'Oh no, not another bloody blog!</h2>
 
 <figure>
-	<img alt='Borat' src='great-success.png'>
-	<figcaption>HIGH FIVE!</figcaption>
+	<img alt="Road through the tussock on the Beebys range" src='images/road-to-nowhere.jpg'>
+	<figcaption>Road to nowhere - Beebys Knob, <a href="https://www.doc.govt.nz/parks-and-recreation/places-to-go/nelson-tasman/places/nelson-lakes-national-park/?tab-id=50578">Nelson Lakes National Park</a>, New Zealand.</figcaption>
 </figure>
 
-<p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p>At this stage most of the content on this site will be in the blogging section.</p>
+<hr />
